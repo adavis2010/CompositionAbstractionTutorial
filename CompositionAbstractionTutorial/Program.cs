e74959df3915088199ada746b7c1aac18fc678f4
max-student <max-student@maxtrain.com>
2021-02-17 13:32:01 -0500

    Add project files.

---
+++ CompositionAbstractionTutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompositionAbstractionTutorial {
+    class Program {
+        static void Main(string[] args) {
+
+            var rect1 = new Rect(3, 8);
+            Console.WriteLine($"Perimeter is {rect1.Perimeter()}");
+            Console.WriteLine($"Area is {rect1.Area()}");
+
+            var sqr1 = new Sqr(4);
+            Console.WriteLine($"Perimeter is {sqr1.Perimeter()}");
+            Console.WriteLine($"Area is {sqr1.Area()}");
+        }
+
+
+
+    }
+}
